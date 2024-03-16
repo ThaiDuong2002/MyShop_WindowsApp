b3ddc466ef387fa037ead5a8b2e08be983a1a3a7
Dương Chí Thông <20127634@student.hcmus.edu.vn>
2024-03-16 08:18:24 +0700

    Hoàn thành giao diện dashboard

---
+++ MyShopProject/Dashboard.xaml.cs
@@ -0,0 +1,65 @@
+﻿using MyShopProject.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyShopProject.View
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1200;
+                    this.Height = 750;
+
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+
+                }
+            }
+        }
+
+        private void MenuButton_Loaded(object sender, RoutedEventArgs e)
+        {
+            var item = sender as MenuButton;
+            Debug.WriteLine(item.btn.IsFocused);
+        }
+    }
+}
